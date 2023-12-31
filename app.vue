d216ea3dac381f60c3c1867e51f682f4fa337d0a
Manuel Yang <me@manuelyang.com>
2023-12-31 15:23:07 -0800

    add tracking

--- app.vue
+++ app.vue
@@ -1,7 +1,14 @@
 <script setup>
 useHead({
-  title: 'Checkify.so'
-})
+  title: 'Checkify.so',
+  script: [
+    {
+      src: 'https://umami-production-c952.up.railway.app/script.js',
+      async: true,
+      'data-website-id': 'd57b10bc-9af3-4710-a7ba-8f3d02a0d418'
+    }
+  ]
+});
 </script>
 
 <template>
@@ -10,12 +17,14 @@ useHead({
   </NuxtLayout>
 </template>
 
-<style lang='scss'>
+<style lang="scss">
 @import 'App.scss';
 
 // Global Overrides
 @keyframes spin {
-    to {transform:rotate(360deg);}
+  to {
+    transform: rotate(360deg);
+  }
 }
 
 .p-icon-spin {
